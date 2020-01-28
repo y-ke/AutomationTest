089f624c70b6d097a5487ad72266063d2609e55b
y-ke <yujingke2012@gmail.com>
2020-01-28 11:17:12 -0800

    Allow user input version but no unittest

---
+++ automationTestWithInput.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+
+
+def searchRank(searchInput, targetSite):
+    searchKeywords = searchInput.replace(' ', '+')
+
+    browser = webdriver.Chrome("/Users/yujingke/Documents/GitHub/AutomationTest/chromedriver")
+
+    pages = 2
+
+    for i in range(pages):
+
+        browser.get("https://www.google.com/search?q=" + searchKeywords + "&start=" + str(i * 10))
+        urlList = []
+
+        for el in browser.find_elements_by_class_name("r"):
+            a = el.find_elements_by_tag_name('a')
+            urlList.append(a[0].get_attribute("href"))
+            print(a[0].tag_name, '--', a[0].text, a[0].get_attribute("href"), '\n')
+            if a[0].get_attribute("href") == targetSite: 
+                print(urlList)
+                print('The rank on the page is ', len(urlList))
+                print('The page number is ', i + 1)
+                
+                return [i + 1, len(urlList)]
+    return 0
+
+searchInput = input("Enter the search keyword: ")
+targetSite = input("Enter the target website (begin with https and end with a /): ")
+searchRank(searchInput, targetSite)
