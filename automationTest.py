d1642c717f5ff45905b11692afd9af37ecab03db
y-ke <yujingke2012@gmail.com>
2020-01-28 11:08:30 -0800

    Supported Chrome only now

---
+++ automationTest.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+import unittest
+
+
+def searchRank(searchInput, targetSite):
+    searchKeywords = searchInput.replace(' ', '+')
+
+    browser = webdriver.Chrome("/Users/yujingke/Documents/GitHub/AutomationTest/chromedriver")
+
+    pages = 2
+
+    for i in range(pages):
+
+        browser.get("https://www.google.com/search?q=" + searchKeywords + "&start=" + str(i * 10))
+        urlList = []
+
+        for el in browser.find_elements_by_class_name("r"):
+            a = el.find_elements_by_tag_name('a')
+            urlList.append(a[0].get_attribute("href"))
+            print(a[0].tag_name, '--', a[0].text, a[0].get_attribute("href"), '\n')
+            if a[0].get_attribute("href") == targetSite: 
+                print(urlList)
+                print('The rank on the page is ', len(urlList))
+                print('The page number is ', i + 1)
+                
+                return [i + 1, len(urlList)]
+    return 0
+
+
+class TestJSONSalesDataMethods(unittest.TestCase):
+
+    def test_jsonSalesData(self):
+        searchInput = "instawork"
+        targetSite = 'https://www.instawork.com/'
+        self.assertEqual(searchRank(searchInput, targetSite), [1, 1])
+
+   
+
+if __name__ == '__main__':
+    unittest.main()
